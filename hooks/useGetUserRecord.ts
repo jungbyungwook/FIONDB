813242b6afeb217d1e4849cf9b11181d0005254e
ahn0min <ahn0min95@gmail.com>
2022-09-20 21:34:07 +0900

    feat: 유저의 기본정보, 매치기록, 최고티어 정보를 다루는 custom-hook 추가

---
+++ hooks/useGetUserRecord.ts
@@ -0,0 +1,20 @@
+import { useQuery } from 'react-query';
+import { getUserMatch } from 'api/user/getUserMatch';
+import { getUserTopTier } from 'api/user/getUserTopTier';
+
+const useGetUserRecord = (accessId: string | undefined) => {
+  const userMatchQuery = useQuery(['userMatch'], () => getUserMatch(accessId), {
+    enabled: !!accessId,
+  });
+  const userTopTierQuery = useQuery(
+    ['userTopTier'],
+    () => getUserTopTier(accessId),
+    {
+      enabled: !!accessId,
+    },
+  );
+
+  return { userMatchQuery, userTopTierQuery };
+};
+
+export default useGetUserRecord;
