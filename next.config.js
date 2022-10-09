9b2ced98bc8d015cd508df7cbf5baa5edd068163
YeongMin Ahn <ahn0min95@gmail.com>
2022-10-09 14:49:33 +0900

    chore: 외부 host로부터 이미지 파일을 받아올 수 있도록 config 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['ssl.nexon.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
