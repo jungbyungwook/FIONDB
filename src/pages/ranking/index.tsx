b2a10dfaf4f2d60c457433f41da8b90ad447a166
jongyeol12 <ljy505541@naver.com>
2022-10-16 21:52:32 +0900

    feat: 랭킹페이지 추가

---
+++ src/pages/ranking/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const Ranking: NextPage = () => {
+  return (
+    <>
+      
+      <RankingWrapper>
+        <h1>ranking</h1>
+      </RankingWrapper>
+    </>
+  );
+};
+
+export default Ranking;
+
+const RankingWrapper = styled.div`
+  display: flex;
+  color: white;
+  flex-direction: column;
+  align-items: center;
+  height: 744px;
+  padding-top:100px;
+`;
