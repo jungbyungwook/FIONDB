c8340d327e93acb3c7d77470844c4cac40069546
jungbyungwook <korjby400@gmail.com>
2022-11-20 19:00:10 +0900

    chore: github action test

--- out/_next/static/SGjLbQq106NcF2-f4Iq8B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c6325db4f7ed4605.js"],"/_error":["static/chunks/pages/_error-5b7b555486245cb3.js"],"/ranking":["static/chunks/pages/ranking-5c1b82046c893e19.js"],sortedPages:["/","/_app","/_error","/ranking"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
