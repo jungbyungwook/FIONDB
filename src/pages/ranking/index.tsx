4c95b18bab80bb015c824914c3838a160aa16085
jongyeol12 <ljy505541@naver.com>
2022-12-31 04:22:54 +0900

    feat: 유저 랭킹 초안 구현, 더미데이터 추가

--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -3,11 +3,73 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styled from 'styled-components';
 
+const dummyData = [
+  {
+    ranking: 1,
+    profileImg: '프로필 이미지',
+    nickname: '닉네임',
+    record: 'nn전 n승 n패',
+    Odds: '50%',
+    rating: '등급1',
+  },
+  {
+    ranking: 2,
+    profileImg: '프로필 이미지',
+    nickname: '닉네임',
+    record: 'nn전 n승 n패',
+    Odds: '50%',
+    rating: '등급2',
+  },
+  {
+    ranking: 3,
+    profileImg: '프로필 이미지',
+    nickname: '닉네임',
+    record: 'nn전 n승 n패',
+    Odds: '50%',
+    rating: '등급3',
+  },
+];
+
+const UserRankingBox = (props: any) => {
+  const { ranking, profileImg, nickname, record, Odds, rating } = props;
+
+  return (
+    <UserRankingBoxWrapper>
+      <ul>
+        <li>{ranking}</li>
+        {/* <li>{profileImg}</li> */}
+        <li>{nickname}</li>
+        <li>{record}</li>
+        <li>{Odds}</li>
+        <li>{rating}</li>
+      </ul>
+    </UserRankingBoxWrapper>
+  );
+};
+
 const Ranking: NextPage = () => {
   return (
     <>
       <RankingWrapper>
-        <h1>ranking 작업중</h1>
+        <h1>랭킹점수</h1>
+        <ul>
+          <li>순위</li>
+          <li>구단주</li>
+          <li>전적</li>
+          <li>승률</li>
+          <li>최고등급1</li>
+        </ul>
+        {dummyData.map((i) => (
+          <UserRankingBox
+            ranking={i.ranking}
+            profileImg={i.profileImg}
+            nickname={i.nickname}
+            record={i.record}
+            Odds={i.Odds}
+            rating={i.rating}
+          />
+        ))}
+        ;
       </RankingWrapper>
     </>
   );
@@ -22,4 +84,30 @@ const RankingWrapper = styled.div`
   align-items: center;
   height: 744px;
   padding-top: 100px;
+
+  ul {
+    list-style: none;
+    color: white;
+    font-size: 20px;
+  }
+  li {
+    float: left;
+  }
+  li + li {
+    margin-left: 150px;
+  }
+`;
+
+const UserRankingBoxWrapper = styled.div`
+  width: 1200px;
+  height: 127.83px;
+  margin-bottom: 16px;
+  background: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ul {
+    color: black;
+  }
 `;
