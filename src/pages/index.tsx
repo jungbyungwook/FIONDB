993cc235254fa6568296d9b4e91c0e2a8fc9820b
jongyeol12 <ljy505541@naver.com>
2023-01-08 16:14:29 +0900

    feat: 전적 보기 버튼 추가, 배경 수정, 사이즈 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,17 +23,18 @@ const Home: NextPage = () => {
               src="/images/main-banner.png"
               alt="메인 배너 이미지"
               layout="fill"
-              objectFit="cover"
-              objectPosition="center"
             />
           </MainBanner>
           <InputWrapper>
             <Input
-              placeholder="닉네임을 입력해주세요"
+              placeholder="구단주 이름을 입력해주세요."
               isFocus={true}
               onChange={onChangeWithNickName}
               onKeyDown={routerPushOnKeyDown}
+              style={{ paddingLeft: 24, fontSize: 18 }}
             />
+
+            <SearchBtn>전적 보기</SearchBtn>
           </InputWrapper>
         </HomeWrapper>
       </Layout>
@@ -59,12 +60,28 @@ const HomeWrapper = styled.div`
 `;
 const MainBanner = styled.div`
   position: relative;
-  width: 48.5rem;
-  height: 25.4rem;
-  margin-top: 15rem;
-  margin-bottom: 5rem;
+  width: 772px;
+  height: 373px;
+  margin-top: 195px;
+  margin-bottom: 80px;
 `;
 const InputWrapper = styled.div`
-  width: 35rem;
-  height: 4.5rem;
+  width: 772px;
+  height: 73px;
+  display: flex;
+`;
+
+const SearchBtn = styled.div`
+  display: flex;
+  margin-left: 13px;
+  width: 174px;
+  height: 73px;
+  background: #212121;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
 `;
