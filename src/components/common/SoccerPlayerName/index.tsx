919e0154dfe8f9c8ca30d22ae98d313c228d4555
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-25 14:50:17 +0900

    Fix: 선수 이름에 띄어쓰기가 없는 경우 렌더링하지 못하는 문제 해결

--- src/components/common/SoccerPlayerName/index.tsx
+++ src/components/common/SoccerPlayerName/index.tsx
@@ -2,6 +2,7 @@ import { useSoccerPlayerName } from 'src/hooks/query/useSoccerPlayerName';
 
 export const SoccerPlayerName = ({ spId }: { spId: number }) => {
   const name = useSoccerPlayerName(spId);
-  const lastName = name?.result.split(' ')[1];
+  // const junkedNames =
+  const lastName = name?.result.split(' ')[1] || name?.result.split(' ')[0];
   return <span>{lastName}</span>;
 };
