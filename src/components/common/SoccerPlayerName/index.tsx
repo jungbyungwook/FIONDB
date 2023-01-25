0d8fc3fc4b60fe5120888b8c74989f8b6c63de06
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-25 14:50:56 +0900

    Chore: 주석제거

--- src/components/common/SoccerPlayerName/index.tsx
+++ src/components/common/SoccerPlayerName/index.tsx
@@ -2,7 +2,7 @@ import { useSoccerPlayerName } from 'src/hooks/query/useSoccerPlayerName';
 
 export const SoccerPlayerName = ({ spId }: { spId: number }) => {
   const name = useSoccerPlayerName(spId);
-  // const junkedNames =
+
   const lastName = name?.result.split(' ')[1] || name?.result.split(' ')[0];
   return <span>{lastName}</span>;
 };
