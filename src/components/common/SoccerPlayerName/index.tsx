a248ea1d0f0dd87218901898e40d0d9d523ceefb
YeongMin Ahn <ahn0min95@gmail.com>
2023-01-25 14:13:57 +0900

    Feat: fullname이 아닌 lastname만을 사용하도록 변경

--- src/components/common/SoccerPlayerName/index.tsx
+++ src/components/common/SoccerPlayerName/index.tsx
@@ -2,5 +2,6 @@ import { useSoccerPlayerName } from 'src/hooks/query/useSoccerPlayerName';
 
 export const SoccerPlayerName = ({ spId }: { spId: number }) => {
   const name = useSoccerPlayerName(spId);
-  return <span>{name?.result}</span>;
+  const lastName = name?.result.split(' ')[1];
+  return <span>{lastName}</span>;
 };
